8ac376a158afc87925abdd76ee9ec734aeb07cf4
Evan Pollack <epollack@salesforce.com>
2023-06-28 18:02:06 -0400

    created modal, started on flow of buttons

--- force-app/main/default/lwc/recorderModal/recorderModal.js
+++ force-app/main/default/lwc/recorderModal/recorderModal.js
@@ -1,3 +1,23 @@
 import { LightningElement } from 'lwc';
 
-export default class RecorderModal extends LightningElement {}
+export default class RecorderModal extends LightningElement {
+    recordingStopped;
+    recording = false;
+    
+    closeModal() {
+        this.dispatchEvent(new CustomEvent('close'));
+    }
+
+    toggleRecording() {
+        this.recording = !this.recording;
+    }
+
+    startRecording() {
+        this.toggleRecording();
+    }
+
+    stopRecording() {
+        this.toggleRecording();
+        this.recordingStopped = true;
+    }
+}
