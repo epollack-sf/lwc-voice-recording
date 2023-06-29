a830ec019092a8130f40f0150c8ab3ddf1c4fee0
Evan Pollack <epollack@salesforce.com>
2023-06-29 10:06:24 -0400

    connected MediaRecorder

--- force-app/main/default/lwc/recorderModal/recorderModal.js
+++ force-app/main/default/lwc/recorderModal/recorderModal.js
@@ -1,8 +1,27 @@
 import { LightningElement } from 'lwc';
 
 export default class RecorderModal extends LightningElement {
+    // mediaEnabled;
+    mediaRecorder;
+    chunks;
+    
     recordingStopped;
     recording = false;
+
+    async connectedCallback() {
+        if (!navigator.mediaDevices) {
+            console.log('Media devices not supported');
+            // this.mediaEnabled = false;
+            return;
+        }
+
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+            this.mediaRecorder = new MediaRecorder(stream);
+        } catch(e) {
+            console.error(`An error occurred: ${e.body.message}`);
+        }
+    }
     
     closeModal() {
         this.dispatchEvent(new CustomEvent('close'));
